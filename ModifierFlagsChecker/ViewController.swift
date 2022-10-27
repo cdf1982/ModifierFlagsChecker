6bcdb03207d06258b697aeec742be1d73878657b
Cesare Forelli <cdf1982@icloud.com>
2022-10-27 17:13:01 +0200

    Check and display if Shift modifier is detected when mouse pointers enters the window Works as expected dn previous macOS versions, but not on Ventura

--- ModifierFlagsChecker/ViewController.swift
+++ ModifierFlagsChecker/ViewController.swift
@@ -8,19 +8,54 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    @IBOutlet weak var textView: NSTextView!
+    
+    var trackingArea : NSTrackingArea?
+    
+    
+    func createTrackingArea() {
+        
+        if trackingArea != nil {
+            
+            self.view.removeTrackingArea(trackingArea!)
+        }
+        
+        trackingArea = NSTrackingArea(rect: self.view.bounds, options: [.mouseEnteredAndExited, .activeAlways], owner: self, userInfo: nil)
+        
+        self.view.addTrackingArea(trackingArea!)
+    }
 
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        createTrackingArea()
+        
+        textView.isEditable = false
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    override func mouseEntered(with event: NSEvent) {
+
+        if let flags = NSApp.currentEvent?.modifierFlags {
+            
+            var logMessage : String
+            
+            if flags.contains([.shift]) {
+                
+                logMessage = "✅ \(Date()) - Shift key detected. NSApp.currentEvent?.modifierFlags: \(String(describing: NSApp.currentEvent?.modifierFlags))"
+                
+            } else {
+                
+                logMessage = "❌ \(Date()) - Shift key not detected. NSApp.currentEvent?.modifierFlags: \(String(describing: NSApp.currentEvent?.modifierFlags))"
+            }
+            
+            print(logMessage)
+            
+            textView.string = "\(logMessage)\n\(textView.string)"
         }
     }
-
-
 }
 
